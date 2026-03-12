e6067802fee4847f53c6512d37357ed5aa568720
koydev <koyedev@gmail.com>
2026-03-12 22:42:31 +0000

    Added changes for staging

---
+++ azure-app-koye-VS/Pages/DeployementSlots.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_koye_VS.Pages
+{
+    public class DeployementSlotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
